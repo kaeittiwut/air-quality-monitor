123a63c3cce165fbea5807fb9384305421c13ee1
Kaeittiwut Thongwhan <kaeitiwutt@gmail.com>
2020-10-20 02:55:07 +0700

    add multi series area chart and fetch data from firebase

--- src/components/monitor/Canvas.js
+++ src/components/monitor/Canvas.js
@@ -1,61 +1,171 @@
 import React, { Component } from 'react';
 import CanvasJSReact from '../../js/canvasjs.react';
-// var CanvasJS = CanvasJSReact.CanvasJS;
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-var dps = [
-  //   { x: 1, y: 10 },
-  //   { x: 2, y: 13 },
-  //   { x: 3, y: 18 },
-  //   { x: 4, y: 20 },
-  //   { x: 5, y: 17 },
-  //   { x: 6, y: 10 },
-  //   { x: 7, y: 13 },
-  //   { x: 8, y: 18 },
-  //   { x: 9, y: 20 },
-  //   { x: 10, y: 17 },
-]; //dataPoints.
-var xVal = dps.length + 1;
-var yVal = 15;
-var updateInterval = 1000;
+import firebase from '../../firebase';
+import $ from 'jquery';
+
+var CanvasJS = CanvasJSReact.CanvasJS;
+
 class Canvas extends Component {
-  constructor() {
-    super();
-    this.updateChart = this.updateChart.bind(this);
-  }
   componentDidMount() {
-    this.timerID = setInterval(this.updateChart, updateInterval);
-  }
+    var chart, toOffline;
+    var newItems = false;
 
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
+    $(document).ready(function (e) {
+      chart = new CanvasJS.Chart('chartContainer', {
+        animationEnabled: true,
+        title: {
+          text: 'PM Detector Chart',
+        },
+        axisX: {
+          valueFormatString: 'HH:mm',
+        },
+        axisY: {
+          valueFormatString: '0.0#',
+        },
+        toolTip: {
+          shared: true,
+          contentFormatter: function (e) {
+            var content =
+              CanvasJS.formatDate(
+                e.entries[0].dataPoint.x,
+                'D DDD YYYY <br> HH:mm:ss'
+              ) + '<br>';
+            content +=
+              '<strong style="color:rgba(40,175,101,0.7)">PM1:</strong> ' +
+              e.entries[0].dataPoint.y +
+              ' µm<br>';
+            content +=
+              '<strong style="color:rgba(40,175,101,0.9)">PM2.5:</strong> ' +
+              e.entries[1].dataPoint.y +
+              ' µm<br>';
+            content +=
+              '<strong style="color:rgba(0,75,141,0.7)">PM10:</strong> ' +
+              e.entries[2].dataPoint.y +
+              ' µm';
+            return content;
+          },
+        },
+        legend: {
+          verticalAlign: 'top',
+          horizontalAlign: 'right',
+          dockInsidePlotArea: false,
+        },
+        dataPointWidth: 10,
+        zoomEnabled: true,
+        data: [
+          {
+            name: 'PM1',
+            showInLegend: true,
+            legendMarkerType: 'square',
+            type: 'area',
+            color: 'rgba(40,175,101,0.5)',
+            markerSize: 0,
+            dataPoints: [],
+          },
+          {
+            name: 'PM2.5',
+            showInLegend: true,
+            legendMarkerType: 'square',
+            type: 'area',
+            color: 'rgba(40,175,101,0.7)',
+            markerSize: 0,
+            dataPoints: [],
+          },
+          {
+            name: 'PM10',
+            showInLegend: true,
+            legendMarkerType: 'square',
+            type: 'area',
+            color: 'rgba(0,75,141,0.5)',
+            markerSize: 0,
+            dataPoints: [],
+          },
+        ],
+      });
+      chart.render();
 
-  updateChart() {
-    yVal = yVal + Math.round(5 + Math.random() * (-5 - 5));
-    dps.push({ x: xVal, y: yVal });
-    xVal++;
-    if (dps.length > 30) {
-      dps.shift();
-    }
-    this.chart.render();
+      var Sensor = firebase.database().ref().child('Sensor');
+
+      Sensor.on('child_added', function (sanp) {
+        if (!newItems) return;
+        var row = sanp.val();
+
+        row.time = new Date(row.time);
+        chart.options.data[0].dataPoints.push({ x: row.time, y: row.PM10 });
+        chart.options.data[1].dataPoints.push({ x: row.time, y: row.PM25 });
+        chart.options.data[2].dataPoints.push({ x: row.time, y: row.PM100 });
+        chart.render();
+
+        $('#time > .contentDate1').html(row.time);
+        $('#PM10 > .content').text(row.PM10 + ' µm');
+        $('#PM25 > .content').text(row.PM25 + ' µm');
+        $('#PM100 > .content').text(row.PM100 + ' µm');
+
+        $('#status').removeClass('danger').addClass('success');
+        $('#status > .content').text('ONLINE');
+
+        setTimeoffline();
+      });
+
+      var now = new Date();
+      Sensor.orderByChild('time')
+        .startAt(
+          now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate()
+        )
+        .once('value', function (sanp) {
+          //   console.log(sanp);
+          newItems = true;
+          var dataRows = sanp.val();
+          var lastRows = 0;
+          $.each(dataRows, function (index, row) {
+            row.time = new Date(row.time);
+            chart.options.data[0].dataPoints.push({ x: row.time, y: row.PM10 });
+            chart.options.data[1].dataPoints.push({ x: row.time, y: row.PM25 });
+            chart.options.data[2].dataPoints.push({
+              x: row.time,
+              y: row.PM100,
+            });
+            lastRows = row;
+          });
+          chart.render();
+
+          var Arow = lastRows;
+          $('#PM10 > .content').text(Arow.PM10 + ' µm');
+          $('#PM25 > .content').text(Arow.PM25 + ' µm');
+          $('#PM100 > .content').text(Arow.PM100 + ' µm');
+
+          var now = new Date();
+          Arow.time = new Date(Arow.time);
+          if (Math.round(now) - Math.round(Arow.time) < 60 * 60 * 10) {
+            $('#status').removeClass('danger').addClass('success');
+            $('#status > .content').text('ONLINE');
+          } else {
+            $('#status').removeClass('success').addClass('danger');
+            $('#status > .content').text('OFFLINE');
+          }
+
+          setTimeoffline();
+        });
+    });
+
+    var setTimeoffline = function () {
+      if (typeof toOffline === 'number') clearTimeout(toOffline);
+
+      toOffline = setTimeout(function () {
+        $('#status').removeClass('success').addClass('danger');
+        $('#status > .content').text('OFFLINE');
+      }, 60 * 60 * 10);
+    };
   }
 
   render() {
-    const options = {
-      title: {
-        text: 'Dynamic Line Chart',
-      },
-      data: [
-        {
-          type: 'line',
-          dataPoints: dps,
-        },
-      ],
-    };
     return (
       <div>
-        <CanvasJSChart options={options} onRef={(ref) => (this.chart = ref)} />
-        {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+        <div
+          id="chartContainer"
+          style={{ height: '400px', width: '100%' }}
+        ></div>
+        <br />
       </div>
     );
   }
